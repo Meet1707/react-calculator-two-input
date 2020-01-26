5436c647151d9c288a59133c2fafab61af5d5901
Meet panchal <meetpanchal1707@gmail.com>
2020-01-26 14:23:26 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    value1: '0',
+    value2: '0',
+    answer: ''
+  }
+  updateState = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  calculateAnswer = (event) => {
+    const { value1, value2 } = this.state;
+    switch (event.target.value) {
+      case "+":
+        this.setState({ answer: Number(value1) + Number(value2) })
+        break;
+      case "-":
+        this.setState({ answer: Number(value1) - Number(value2) })
+        break;
+      case "*":
+        this.setState({ answer: Number(value1) * Number(value2) })
+        break;
+      case "/":
+        this.setState({ answer: Number(value1) / Number(value2) })
+        break;
+    }
+  }
+  render() {
+    return (
+      <>
+        <h5>Calculator</h5>
+        <div className="row">
+          <div className="col">
+            <input
+              className="form-control"
+              placeholder="Enter value"
+              type="text"
+              name="value1"
+              onChange={e => this.updateState(e)}
+            />
+          </div>
+          <div className="col">
+            <input
+              className="form-control"
+              placeholder="Enter value"
+              type="text"
+              name="value2"
+              onChange={e => this.updateState(e)}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <button className="btn btn-dark calcButton"
+              value="+" onClick={e => this.calculateAnswer(e)}>
+              +</button>
+          </div>
+          <div className="col">
+            <button className="btn btn-dark calcButton"
+              value="-" onClick={e => this.calculateAnswer(e)}>
+              -</button>
+          </div>
+          <div className="col">
+            <button className="btn btn-dark calcButton"
+              value="*" onClick={e => this.calculateAnswer(e)}>
+              *</button>
+          </div>
+          <div className="col">
+            <button className="btn btn-dark calcButton"
+              value="/" onClick={e => this.calculateAnswer(e)}>
+              /</button>
+          </div>
+        </div>
+        <p className="form-control">Answer: {this.state.answer}</p>
+      </>
+    );
+  }
 }
 
 export default App;
